b00e71f44730cc4a8491302ccc821423bef3987b
SAHA-YJB <pkomom123@naver.com>
2023-12-20 14:07:40 +0900

    미들웨어 기본 적용 요청과 응답 시간 확인 로그

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 //  res.sned() : 문자열을 보내는 메서드 -> 내부적으로 호출이 한번 더 일어남
 //  res.json() : json을 보내는 메서드 -> send보다 호출이 한번 적음
 //  객체를 보낼 때는 json을 사용하는 것이 직관적
+//  res.json이나 res.send는 알아서 종료됨 end()를 따로 호출할 필요 없음
 
 import express, { Request, Response } from 'express';
+//익스프레스 앱 생성
+const app = express();
 
 const PORT = 4000;
 const Users = [
@@ -20,8 +23,16 @@ const Users = [
   },
 ];
 
-//익스프레스 앱 생성
-const app = express();
+// 미들웨어 등록
+// 로그를 남기는 미들웨어
+app.use((req: Request, res: Response, next) => {
+  const start = Date.now();
+  console.log(`[serverMiddleware]: ${req.method} ${req.url}`);
+  next();
+  const diffTime = Date.now() - start;
+  // 요청부터 응답까지의 시간을 측정
+  console.log(`[start-end]: ${req.method} ${req.url} ${diffTime}ms`);
+});
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
@@ -29,7 +40,7 @@ app.get('/', (req: Request, res: Response) => {
 
 //유저 목록 조회 API
 app.get('/users', (req: Request, res: Response) => {
-  res.send(Users);
+  res.json(Users);
 });
 
 //유저 상세 조회 API
